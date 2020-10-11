6ffd5b3c0d7e06819e93f9431d0ea0c25caf7837
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-10-11 20:40:47 +0700

    Update task_1_1.clj

--- lab_1/task_1_1.clj
+++ lab_1/task_1_1.clj
@@ -1,32 +1,32 @@
 (ns my-stuff.core
   (:gen-class))
 
-(defn connect_symbol_alphabet [symbol alphabet]
-    (if (> (count alphabet) 0)
-      (if (= symbol (list (last (last (list (first alphabet))))))
-        (connect_symbol_alphabet symbol (rest alphabet))
-        (cons (concat (first alphabet) symbol) (connect_symbol_alphabet symbol (rest alphabet))))
-      ()))
-;(connect_symbol_alphabet '("d") '(("a" "b") ("b" "c") ("c" "d")))
-
-(defn combine_alphabets [alphabet original_alphabet]
-  (if (> (count original_alphabet) 0)
-    (concat (connect_symbol_alphabet (first original_alphabet) alphabet) (combine_alphabets alphabet (rest original_alphabet)))
-    ()))
-;(combine_alphabets '(("a") ("b") ("c")) '(("a") ("b") ("c")))
-
-(defn count_n [alphabet original_alphabet n]
-    (if (> n 1)
-      (count_n (combine_alphabets alphabet original_alphabet) original_alphabet (dec n))
-      alphabet))
-;(count_n '(("a") ("b") ("c")) '(("a") ("b") ("c")) 3)
-
-(defn list_to_list [alphabet]
+(defn connect-symbol-alphabet [symbol alphabet]
   (if (> (count alphabet) 0)
-    (cons (list (first alphabet)) (list_to_list (rest alphabet)))
+    (if (= symbol [(first (first alphabet))])
+      (connect-symbol-alphabet symbol (rest alphabet))
+      (cons (concat symbol (first alphabet)) (connect-symbol-alphabet symbol (rest alphabet))))
     ()))
-;(list_to_list '("a" "b" "c"))
+;(connect-symbol-alphabet '("d") '(("a" "b") ("b" "c") ("d" "c")))
 
-(defn task_1_1 [alphabet n]
-  (count_n (list_to_list alphabet) (list_to_list alphabet) n))
-;(task_1_1 '("a" 1 :b) 2)
+(defn combine-alphabets [alphabet original-alphabet]
+  (if (> (count original-alphabet) 0)
+    (concat (connect-symbol-alphabet (first original-alphabet) alphabet) (combine-alphabets alphabet (rest original-alphabet)))
+    ()))
+;(combine-alphabets '(("a") ("b") ("c")) '(("a") ("b") ("c")))
+
+(defn count-n [alphabet original-alphabet n]
+    (if (> n 1)
+      (count-n (combine-alphabets alphabet original-alphabet) original-alphabet (dec n))
+      alphabet))
+;(count-n '(("a") ("b") ("c")) '(("a") ("b") ("c")) 3)
+
+(defn list-to-list [alphabet]
+  (if (> (count alphabet) 0)
+    (cons (list (first alphabet)) (list-to-list (rest alphabet)))
+    ()))
+;(list-to-list '("a" "b" "c"))
+
+(defn task-1-1 [alphabet n]
+  (count-n (list-to-list alphabet) (list-to-list alphabet) n))
+;(task-1-1 '("a" 1 :b) 2)
