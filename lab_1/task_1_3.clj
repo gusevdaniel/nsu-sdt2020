64348b25d55c39ed6b6bcab13e2fbff73227af58
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-10-11 20:40:59 +0700

    Update task_1_3.clj

--- lab_1/task_1_3.clj
+++ lab_1/task_1_3.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (defn my-map [f coll]
-  (reduce (fn [new_coll value] (concat new_coll [(f value)])) [] coll))
+  (reduce (fn [new_coll value] (conj new_coll (f value))) [] coll))
 
 (defn my-filter [f coll]
-  (reduce (fn [new_coll value] (if (f value) (concat new_coll [value]) new_coll)) [] coll))
+  (reduce (fn [new_coll value] (if (f value) (conj new_coll value) new_coll)) [] coll))
