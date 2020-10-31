57a701c205977f3f6600e128319a4889d68b626b
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-10-31 22:32:28 +0700

    Update task_2_2.clj

--- lab2/task_2_2.clj
+++ lab2/task_2_2.clj
@@ -1,18 +1,20 @@
 (ns task-2-2.core
   (:gen-class))
 
-(defn integral_flow [f step]
-  (map first (iterate
-              (fn [[sum x]] [(+ sum (* (* (+ (f x) (f (+ x step))) 0.5) step)) (+ x step)]) [0.0 0.0])))
-;(nth (integral_flow #(* % %) 0.1) 50)
+(defn create-seq [f a step]
+(map first (iterate
+            (fn [[sum x]] [(+ sum (* (* (+ (f x) (f (+ x step))) 0.5) step)) (+ x step)]) [0.0 a])))
 
-(defn integral [f x step]
-  (nth (integral_flow f step) (/ x step)))
-;(integral #(* % %) 5 0.1)
+(defn get-integrator [f a step]
+  (let [seq (create-seq f a step)]
+    (fn [b]
+      (nth seq (/ b step)))))
 
 (defn -main []
-  (let [f #(* % %) step 0.1]
-    (time (integral f 5 step))
-    (time (integral f 5 step))
-    (time (integral f 6 step))))
+  (let [integrator (get-integrator #(* % %) 0 0.01)]
+    (time (integrator 500))
+    (time (integrator 500))
+    (time (integrator 501))
+    (time (integrator 520))
+    ))
 (-main)
