213d4770bfe704d46d7ebaaedecfe5b63bb2cb73
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-10-31 22:32:25 +0700

    Update task_2_1.clj

--- lab2/task_2_1.clj
+++ lab2/task_2_1.clj
@@ -1,31 +1,30 @@
 (ns task-2-1.core
   (:gen-class))
 
-(defn trapezoid [f x0 x1 step]
-  (* (* (+ (f x0) (f x1)) 0.5) step))
-;(trapezoid #(* % %) 1 2 1)
+(defn trapezoid [f x0 x1]
+  (* (+ (f x0) (f x1)) 1/2))
+;(trapezoid #(* % %) 1 2)
 
 (defn integral [f x0 x1 n]
   (let [step (/ (- x1 x0) n)]
   (loop [start x0 acc 0]
     (if (< start x1)
-      (recur (+ start step) (+ acc (trapezoid f start (+ start step) step)))
-      acc))))
-;(integral #(* % %) 0 5 10)
+      (recur (+ start step) (+ acc (trapezoid f start (+ start step))))
+      (* acc step)))))
+;(integral #(* % %) 0 5 50)
 
 (def integral-memo
   (memoize
-   (fn [f x1 step]
-     (let [x0 (- x1 step)]
-       (if (> x0 0)
-         (+ (integral-memo f x0 step) (* (* (+ (f x0) (f x1)) 0.5) step))
-         (+ (* (* (+ (f (- x1 step)) (f x1)) 0.5) step)))))))
-;(integral-memo #(* % %) 5 0.1)
+   (fn [f x0 x1 step]
+     (if (< x0 x1)
+         (+ (integral-memo f (+ x0 step) x1 step) (* (* (+ (f x0) (f (+ x0 step))) 1/2) step))
+         (- (* (* (+ (f x0) (f x1)) 1/2) step))))))
+;(integral-memo #(* % %) 0 5 0.1)
 
 (defn -main []
   (let [f #(* % %) x0 0 x1 5]
   (time (integral f x0 x1 50))
-  (time (integral-memo f x1 0.1))
-  (time (integral-memo f x1 0.1))
-  (time (integral-memo f x1 0.1))))
+  (time (integral-memo f x0 x1 0.1))
+  (time (integral-memo f x0 x1 0.1))
+  (time (integral-memo f x0 x1 0.1))))
 (-main)
