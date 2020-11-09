9425b348f0502d16aab858928a6e30fa43f6be00
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-11-09 17:10:05 +0700

    Update task_3_2.clj

--- lab3/task_3_2.clj
+++ lab3/task_3_2.clj
@@ -17,7 +17,7 @@
     (let [batch (take (* number-futures future_size) coll)]
       (->>
        batch
-       (delimiter number-futures)
+       (partition-all number-futures)
        (map #(future (doall (filter f %))))
        (doall)
        (map deref)
@@ -29,17 +29,19 @@
 ;(take 10 (my-lazy-filter (fn [n] (= 0 (mod n 3))) (range) 10 5))
 
 (defn -main []
-  (let
-   [f (fn [n] (= 0 (mod n 3))) heavy-f (heavy 100 f) coll (range 30)]
+  (let [f (fn [n] (= 0 (mod n 3))) heavy-f (heavy 100 f) 
+        coll (range 50) future_size 3 
+        true-elements (count (filter f coll))]
     (println "For limited sequence")
     (time (doall (filter heavy-f coll)))
-    (time (doall (my-lazy-filter heavy-f coll 1 5)))
-    (time (doall (my-lazy-filter heavy-f coll 5 5)))
-    (time (doall (my-lazy-filter heavy-f coll 10 5)))
+    (time (doall (my-lazy-filter heavy-f coll 1 future_size)))
+    (time (doall (my-lazy-filter heavy-f coll 5 future_size)))
+    (time (doall (my-lazy-filter heavy-f coll 10 future_size)))
+    (time (count (doall (my-lazy-filter heavy-f coll 10 future_size))))
     (println "For endless sequence")
-    (time (doall (take 10 (filter heavy-f (range)))))
-    (time (doall (take 10 (my-lazy-filter heavy-f (range) 1 5))))
-    (time (doall (take 10 (my-lazy-filter heavy-f (range) 5 5))))
-    (time (doall (take 10 (my-lazy-filter heavy-f (range) 10 5))))
+    (time (doall (take true-elements (filter heavy-f (range)))))
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 1 future_size))))
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 5 future_size))))
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 10 future_size))))
     ))
 (-main)
