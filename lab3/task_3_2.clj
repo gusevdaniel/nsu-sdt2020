2d33c12a0b513f332d1384398f573d4d16b8a660
gusevdaniel <42457630+gusevdaniel@users.noreply.github.com>
2020-11-10 18:52:00 +0700

    Update task_3_2.clj

--- lab3/task_3_2.clj
+++ lab3/task_3_2.clj
@@ -6,35 +6,43 @@
     (Thread/sleep sleep)
     (apply f coll)))
 
-(defn my-lazy-filter [f coll number-futures future_size]
-   (lazy-cat
-    (let [batch (take (* number-futures future_size) coll)]
-      (->>
-       batch
-       (partition-all (/ (count batch) number-futures))
-       (map #(future (doall (filter f %))))
-       (doall)
-       (map deref)
-       (doall)
-       (apply concat)))
-    (if (> (count (take future_size coll)) 0)
-      (my-lazy-filter f (drop (* number-futures future_size) coll) number-futures future_size)
-      ())))
-;(take 10 (my-lazy-filter (fn [n] (= 0 (mod n 3))) (range) 10 5))
+(defn lazy-delimiter [size-coll coll]
+  (lazy-cat
+   (if (> (count (take size-coll coll)) 0)
+     (cons (take size-coll coll) (lazy-delimiter size-coll (drop size-coll coll)))
+     ())))
+;(lazy-delimiter 2 (range 10))
+
+(defn perform-batch [f]
+  (fn [coll]
+    (->>
+     coll
+     (map #(future (doall (filter f %))))
+     (doall)
+     (map deref)
+     (apply concat)
+     (lazy-seq))))
+
+(defn my-lazy-filter [f coll size_chank size_batch]
+  (->>
+   coll
+   (lazy-delimiter size_chank)
+   (lazy-delimiter size_batch)
+   (map #((perform-batch f) %))
+   (apply concat)))
+;(take 20 (my-lazy-filter (fn [n] (= 0 (mod n 3))) (range) 5 10))
 
 (defn -main []
-  (let [f (fn [n] (= 0 (mod n 3))) heavy-f (heavy 100 f) 
-        coll (range 50) future_size 3 
+  (let [f (fn [n] (= 0 (mod n 3))) heavy-f (heavy 100 f)
+        coll (range 50) size_chank 3
         true-elements (count (filter f coll))]
     (println "For limited sequence")
     (time (doall (filter heavy-f coll)))
-    (time (doall (my-lazy-filter heavy-f coll 1 future_size)))
-    (time (doall (my-lazy-filter heavy-f coll 5 future_size)))
-    (time (doall (my-lazy-filter heavy-f coll 10 future_size)))
+    (time (doall (my-lazy-filter heavy-f coll size_chank 1)))
+    (time (doall (my-lazy-filter heavy-f coll size_chank 5)))
+    (time (doall (my-lazy-filter heavy-f coll size_chank 10)))
     (println "For endless sequence")
     (time (doall (take true-elements (filter heavy-f (range)))))
-    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 1 future_size))))
-    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 5 future_size))))
-    (time (doall (take true-elements (my-lazy-filter heavy-f (range) 10 future_size))))
-    ))
-(-main)
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) size_chank 1))))
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) size_chank 5))))
+    (time (doall (take true-elements (my-lazy-filter heavy-f (range) size_chank 10))))))
